59d87bf89a391fab9e04d86380d3b5a1a2c16196
Sviat-cpp <grecanksvat@gmail.com>
2026-08-26 15:37:55 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+ function lidl() {
+    const name=document.getElementById("nameInput").value;
+    const age=document.getElementById("qwerty");
+    age.innerText="Hello,"+ name + "!?";
+ }
+ function goprojekt() {
+    lidl();
+    
+    
+ }
